c740e520bc4077cdecbc4bda554836090907073b
Le Pan <ashley@Les-MacBook-Air.home>
2020-08-11 10:16:15 +0200

    try to customize algolia componments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import algoliasearch from "algoliasearch/lite";
+import {
+  InstantSearch,
+  connectSearchBox,
+  connectHits,
+} from "react-instantsearch-dom";
+import Hits from "./Hits";
+
+const searchClient = algoliasearch(
+  "JMTNZZLDZ9",
+  "c3de62a395cab0ce32181ac24fa2ddda"
+);
+
+const styles = {
+  app: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  form: { display: "flex", justifyContent: "center" },
+  SearchBox: {
+    width: "400px",
+    height: "50px",
+    marginRight: "10px",
+  },
+};
+
+const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
+  <form style={styles.form} noValidate action='' role='search'>
+    <input
+      style={styles.SearchBox}
+      type='search'
+      placeholder='Search Movies'
+      value={currentRefinement}
+      onChange={(event) => refine(event.currentTarget.value)}
+    />
+    <button onClick={() => refine("")}>Clear</button>
+    {isSearchStalled ? "My search is stalled" : ""}
+  </form>
+);
+const CustomSearchBox = connectSearchBox(SearchBox);
+const CustomHits = connectHits(Hits);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.app}>
+      <h1>AlgoliaTest</h1>
+      <InstantSearch searchClient={searchClient} indexName='Hypertube'>
+        <CustomSearchBox autoFocus />
+        <CustomHits />
+      </InstantSearch>
     </div>
   );
 }
---
+++ src/Hits.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const styles = {
+  hitsDiv: {
+    width: "600px",
+    display: "flex",
+    flexDirection: "column",
+    margin: "10px",
+    maxHeight: "500px",
+    overflow: "auto",
+  },
+  decriptionDiv: {
+    display: "flex",
+    flexDirection: "column",
+    paddingLeft: "10px",
+  },
+  hit: {
+    display: "flex",
+    alignItems: "center",
+    margin: "10px",
+  },
+};
+const Hits = ({ hits }) => {
+  return (
+    <div style={styles.hitsDiv}>
+      {hits.map((hit, key) => (
+        <div key={key} style={styles.hit}>
+          <img src={hit.Poster} width='50px' alt={hit.Title}></img>
+          <div style={styles.decriptionDiv}>
+            <b>{hit.Title}</b>
+            <span style={{ color: "grey" }}>{hit.Year}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Hits;
